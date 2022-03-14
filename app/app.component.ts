6b8cd0dee12a05f2ba345685ec140bad39abb8c4
JoseSosaMendez <jose.sosa.mdz@gmail.com>
2022-03-14 14:22:12 -0600

    3 files modified, 1 added

--- app/app.component.ts
+++ app/app.component.ts
@@ -10,23 +10,38 @@ import { multi } from './data';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
+
 export class AppComponent {
   multi: any[];
-  view: any[] = [700, 300];
 
   menu: MatMenuPanel<any>;
 
   // options
-  legend: boolean = true;
+  legend: boolean = false;
   showLabels: boolean = true;
   animations: boolean = true;
   xAxis: boolean = true;
   yAxis: boolean = true;
   showYAxisLabel: boolean = true;
   showXAxisLabel: boolean = true;
+  legentPosition: 'below';
   xAxisLabel: string = 'Year';
   yAxisLabel: string = 'Population';
   timeline: boolean = true;
+  customColors: object[];
 
   colorScheme = {
     domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5'],
@@ -38,5 +53,9 @@ export class AppComponent {
 
   onSelect(event) {
     console.log(event);
+    this.customColors = [{ name: event.name, value: '#9E9E9E' }];
   }
+
+  displayedColumns: string[] = ['Año', 'Personas', 'Pais'];
+  dataSource = 
 }
